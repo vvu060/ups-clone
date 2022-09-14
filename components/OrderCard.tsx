995c9d081391b58423d6e9ecb3ddcb04296134e8
Personal <personal@Vishals-MacBook-Air.local>
2022-09-14 12:25:03 +0530

    ups clone

---
+++ components/OrderCard.tsx
@@ -0,0 +1,64 @@
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Card, Icon } from "@rneui/themed";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useTailwind } from "tailwind-rn/dist";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { TabStackParamList } from "../navigator/TabNavigator";
+
+export type OrdersScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Orders">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+
+type Props = {
+  item: Order;
+};
+
+const OrderCard = ({ item }: Props) => {
+  const tw = useTailwind();
+  const navigation = useNavigation<OrdersScreenNavigationProp>();
+
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.navigate("Order", { order: item })}
+    >
+      <Card containerStyle={tw("px-5 rounded-lg")}>
+        <View style={tw("flex-row justify-between items-center")}>
+          <View>
+            <Icon
+              name="truck-delivery"
+              color="#EB6A7C"
+              type="material-community"
+            />
+            <Text style={{ fontSize: 10 }}>
+              {new Date(item.createdAt).toDateString()}
+            </Text>
+          </View>
+
+          <View>
+            <Text style={[tw("text-gray-400"), { fontSize: 10 }]}>
+              {item.carrier} - {item.trackingId}
+            </Text>
+            <Text style={tw("text-gray-500 text-xl")}>
+              {item.trackingItems.customer.name}
+            </Text>
+          </View>
+
+          <View style={tw("flex-row items-center")}>
+            <Text style={[tw("text-sm"), { color: "#EB6A7C" }]}>
+              {item.trackingItems.items.length} x
+            </Text>
+            <Icon name="box" type="feather" style={tw("ml-2")} />
+          </View>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+export default OrderCard;
---
+++ navigator/TabNavigator.tsx
@@ -0,0 +1,56 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import CustomersScreen from "../screens/CustomersScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+import { Icon } from "@rneui/themed";
+
+export type TabStackParamList = {
+  Customers: undefined;
+  Orders: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
+
+const TabNavigator = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Customers") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "#59C1CC" : "gray"}
+              />
+            );
+          } else if (route.name === "Orders") {
+            return (
+              <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "#EB6A7C" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Customers" component={CustomersScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
